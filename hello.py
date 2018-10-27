ce9fdc7f897d51ff10b1c467cc61fd0b54bfe11f
CodingBear <surfwon@gmail.com>
2018-10-27 22:58:00 +0900

    confilict resolved GitHub

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
 // For command line git tutorial
 // GitHub modification
+// Second: GitHub modification
 print("Hello World")
 print("Tell your world")
 print("Tell his world")
 print("Tell her world")
+
